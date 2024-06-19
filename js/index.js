89ddb671f837ee48d03dd714e8be3bf2d90eb870
muxtorjonov2005 <muxtorjonoveldiyor@gmail.com>
2024-06-19 18:10:40 +0500

    first commiy

---
+++ js/index.js
@@ -0,0 +1,47 @@
+/**
+ * 1-masala
+ * function createColors(color1, color2, color3){
+ * let res []
+ * res.push(color1)
+ * res.push(color2)
+ * res.push(color3)
+ * return res
+ * }
+ */ 
+// console.log(createColors());
+
+/**
+ * 2-masala
+ * function createNumber(num1, num2, num3){
+ * let res =[num1, num2, num3]
+ * return res
+ * }
+ */
+// console.log(createNumber());
+
+// 3-masala
+// function sonlarMassiviYarat() {
+// }
+// console.log(sonlarMassiviYarat[0]);
+// console.log(sonlarMassiviYarat[sonlarMassiviYarat.length-1]);
+
+// 4-masala
+function massivElementOzgartirish (){
+    
+}
+
+// 5-masala
+
+// 6-masala
+
+// 7-masala
+function massivUzunligi() {
+}
+console.log(massivUzunligi.length);
+
+// 8-masala
+
+// 9-masala
+
+// 10-mmasala
+
